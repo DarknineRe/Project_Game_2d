5b49a4126c9b0dcf14fe7e74fef6d21b65e1febc
DarknineRe <chanin.ja@kkumail.com>
2025-09-01 18:51:27 +0700

    a little Change

--- Scene/initiate_arrow.gd
+++ Scene/initiate_arrow.gd
@@ -2,12 +2,17 @@ extends MonsterState
 
 
 var timer : Timer
-@export var damage_attack : float = 10
+@export var damage_attack : float = 5
 
 # Upon moving to this state, initialize timer
 # and stun enemy
 func enter():
 	shoot_arrow()
+	timer = Timer.new()
+	timer.wait_time = 1.0
+	timer.autostart = true
+	timer.timeout.connect(on_timer_finished)
+	add_child(timer)
 
 
 func shoot_arrow():
@@ -23,4 +28,22 @@ func shoot_arrow():
 	arrow.rotation = direction.angle() # 🔥 make arrow point towards player
 	
 	add_child(arrow)
-	
+
+# Upon leaving this state, clear and free all
+# state relevant stuff
+func exit():
+	timer.stop()
+	timer.timeout.disconnect(on_timer_finished)
+	timer.queue_free()
+	timer = null
+
+
+func on_timer_finished():
+	if !try_chase():
+		transitioned.emit(self, "chase")
+
+func attack_hit(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var attack = Attack.new()
+		attack.damage = damage_attack
+		body.damage(attack)
