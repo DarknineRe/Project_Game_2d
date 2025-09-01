fcc8fa5360666ce181713502323580069507bd76
zeemongteng <zeemongteng@gmail.com>
2025-09-01 17:43:42 +0700

    added archer(bugged)

--- Scene/initiate_arrow.gd
+++ Scene/initiate_arrow.gd
@@ -7,36 +7,17 @@ var timer : Timer
 # Upon moving to this state, initialize timer
 # and stun enemy
 func enter():
-	
-	
-	
-	'timer = Timer.new()
-	timer.wait_time = 1.0
-	timer.autostart = true
-	timer.timeout.connect(on_timer_finished)
-	add_child(timer)'
-	
-	
-
-func get_player_vector():
-	##TODO get player vector for shooting bullet
-	pass
-
-# Upon leaving this state, clear and free all
-# state relevant stuff
-func exit():
-	timer.stop()
-	timer.timeout.disconnect(on_timer_finished)
-	timer.queue_free()
-	timer = null
+	shoot_arrow()
 
 
-func on_timer_finished():
-	if !try_chase():
-		transitioned.emit(self, "chase")
-
-func attack_hit(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		var attack = Attack.new()
-		attack.damage = damage_attack
-		body.damage(attack)
+func shoot_arrow():
+	
+	print("arrow shot")
+	
+	var arrow = preload("res://normal_enemy_bullet.tscn").instantiate()
+	var direction := player.global_position - enemy.global_position
+	arrow.global_position = enemy.global_position
+	arrow.direction = (player.global_position - enemy.global_position).normalized()
+	
+	add_child(arrow)
+	
