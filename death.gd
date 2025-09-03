6fda0a2313e35dadc7bfc407ebd32f94797352cc
parinnakorn <parinnakorn.y@kkumail.com>
2025-09-03 10:23:13 +0700

    score

--- death.gd
+++ death.gd
@@ -4,4 +4,7 @@ func enter():
 	super.enter()
 	animation_player.play("death")
 	await animation_player.animation_finished
+	var p = get_tree().get_first_node_in_group("Player") as Player
+	if p:
+		p.add_score(1000)
 	owner.queue_free()
