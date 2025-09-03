2291a84114489640fdadf6a58b8b4fc2626ab35a
DarknineRe <chanin.ja@kkumail.com>
2025-09-03 09:16:18 +0700

    web

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756865135|1295597502';
+const CACHE_VERSION = '1756865744|1904582358';
 /** @type {string} */
 const CACHE_PREFIX = 'Project_Midterm-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
